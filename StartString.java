39351b3bbd8434d3f3cb91239d004c7522faadf6
Anandhu2903 <anandhumohanan2903@gmail.com>
2025-03-11 13:53:01 +0530

    String operations

---
+++ StartString.java
@@ -0,0 +1,10 @@
+class StartString
+{
+    public static void main(String[] rags)
+    {
+        String s1="Good Morning";
+        System.out.println(s1);
+        System.out.println("To upper case : "+s1.toUpperCase());
+        System.out.println("To lower case : "+s1.toLowerCase());
+    }
+}
